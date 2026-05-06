ab8429102b577f69b59e65c576de22779248bd2f
Petr Chalupa <chalupa.petr93@gmail.com>
2026-05-06 21:13:52 +0200

    Run e2e tests on ci (#59)
    * Add e2e ci pipeline
    
    Signed-off-by: pchalupa <chalupa.petr93@gmail.com>
    
    * Use list reporter
    
    Signed-off-by: pchalupa <chalupa.petr93@gmail.com>
    
    * Ignore web server logs
    
    Signed-off-by: pchalupa <chalupa.petr93@gmail.com>
    
    ---------
    
    Signed-off-by: pchalupa <chalupa.petr93@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  reporter: 'list',
   use: {
     baseURL: 'http://localhost:3000',
   },
@@ -9,6 +10,8 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: true,
+    stdout: 'ignore',
+    stderr: 'ignore',
   },
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } }
